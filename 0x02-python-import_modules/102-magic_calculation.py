004ca8b1f57136e82fd18dd9b3a5a5a0dc79c5f7
HundaolBadada <hundaolbadho@gmail.com>
2024-03-14 04:07:25 -0700

    python

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,10 +1,12 @@
-#!/bin/usr/bin/ptyhon3
-
 def magic_calculation(a, b):
-    from magic_calculation_102 import add, sub
-    if (a < b):
+    add, sub = magic_calculation_102.add, magic_calculation_102.sub
+
+    if a < b:
         c = add(a, b)
-        for z in range(4, 6):
-            c = add(c, z)
-        return(c)
-    return(sub(a, b))
+
+        for i in range(4, 6):
+            c = add(c, i)
+
+        return c
+
+    return sub(a, b)
