2ccb2f9aea4c6bba8b240a92be6b951bd12a449d
HundaolBadada <hundaolbadho@gmail.com>
2024-03-17 23:35:37 -0700

    python programming

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-def only_diff_elements(set_1, set_2):
-    return set_1 ^ set_2
-# The symmetrical difference of A and B, denoted “A Δ B”
-# (read “A delta B”) is the set of elements which belong
-# either to A or to B, but not to both at the same time.
-# We can use set_1.symetric_difference(set_2) too.
+def new_in_list(my_list, idx, element):
+    listlength = len(my_list) - 1
+    if (idx < 0 or idx > listlength):
+        return (my_list)
+    else:
+        new_list = my_list[:]
+        new_list[idx] = element
+        return (new_list)
