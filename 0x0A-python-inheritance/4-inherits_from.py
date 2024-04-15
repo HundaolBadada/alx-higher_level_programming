14c77b3c534ac381e836e6647c719f5b1d72ee0b
HundaolBadada <hundaolbadho@gmail.com>
2024-04-15 08:10:41 -0700

    python-inheritance

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-""" Return Only sub class of a class """
+"""
+===================================
+module with method inherits_from
+===================================
+"""
 
 
 def inherits_from(obj, a_class):
-    """
-    Return true if the object is an instance of a class
-    """
-    if isinstance(obj, a_class) and type(obj) != a_class:
-        return True
-    else:
-        return False
+    """Method that return True if an object is an instance of a class
+    that inherited from"""
+
+    return False if type(obj) is a_class else isinstance(obj, a_class)
